8915dfb628308028dc0cd9f0565faf95dabadadb
monk3yd <jegajardog@gmail.com>
2023-05-28 07:40:18 -0300

    Init UFO for folding code

---
+++ lua/custom/plugins/ufo.lua
@@ -0,0 +1,31 @@
+return {
+	'kevinhwang91/nvim-ufo',
+	dependencies = { 'kevinhwang91/promise-async' },
+	config = function()
+		local keymap = vim.keymap.set
+		local ufo = require 'ufo'
+		ufo.setup {
+			open_fold_hl_timeout = 150,
+			close_fold_kinds = { 'imports', 'comment' },
+			preview = {
+				win_config = {
+					border = { '', '─', '', '', '', '─', '', '' },
+					winhighlight = 'Normal:Folded',
+					winblend = 0
+				},
+				mappings = {
+					scrollU = '<C-u>',
+					scrollD = '<C-d>'
+				}
+			},
+			provider_selector = function(bufnr, filetype, buftype)
+				return { 'treesitter', 'indent' }
+			end
+		}
+
+		keymap('n', 'zR', ufo.openAllFolds)
+		keymap('n', 'zM', ufo.closeAllFolds)
+		keymap('n', 'zm', ufo.closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+		keymap('n', 'zr', ufo.openFoldsExceptKinds)
+	end,
+}
