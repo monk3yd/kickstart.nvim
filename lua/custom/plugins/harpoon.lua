7c90991c2a7aeda21f162acda654f84a4eeb191a
monk3yd <jegajardog@gmail.com>
2023-05-28 07:40:05 -0300

    Add description to harpoon keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,9 +10,9 @@ return {
 		vim.keymap.set('n', '<leader>a', mark.add_file, { desc = '[A]dd mark to file' })
 		vim.keymap.set('n', '<leader>f', ui.toggle_quick_menu, { desc = 'Toggle harpoon UI' })
 
-		vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
-		vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
-		vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
-		vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+		vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end, { desc = 'Harpoon into file 1' })
+		vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end, { desc = 'Harpoon into file 2' })
+		vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end, { desc = 'Harpoon into file 3' })
+		vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end, { desc = 'Harpoon into file 4' })
 	end,
 }
