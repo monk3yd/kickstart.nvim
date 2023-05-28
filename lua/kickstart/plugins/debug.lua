813a62676d104c1373d58f643c52a9865481c8cd
monk3yd <jegajardog@gmail.com>
2023-05-28 04:38:04 -0300

    Add descriptions to mappings for fzf
    Better description for FZF

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -43,11 +43,12 @@ return {
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue)
-    vim.keymap.set('n', '<F1>', dap.step_into)
-    vim.keymap.set('n', '<F2>', dap.step_over)
-    vim.keymap.set('n', '<F3>', dap.step_out)
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debugger start/resumes execution' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debugger step into a function or method' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debugger run next step' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debugger step out a function or method' })
+    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint,
+      { desc = 'Debugger create or remove breakpoint at current line' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end)
@@ -74,8 +75,8 @@ return {
       },
     }
     -- toggle to see last session result. Without this ,you can't see session output in case of unhandled exception.
-    vim.keymap.set("n", "<F7>", dapui.toggle)
-    
+    vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Deugger toogle UI" })
+
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
