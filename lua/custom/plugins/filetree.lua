7c2d69ad8d8672cad737033e72516f3dcbabf1ad
monk3yd <jegajardog@gmail.com>
2023-09-18 00:53:49 -0300

    Fix filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,19 +1,26 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
+	version = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		-- Unless you are still migrating, remove the deprecated commands from v1.x
 		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-		local neotree = require 'neo-tree'
-		neotree.setup {}
-
-		vim.keymap.set("n", "<Space>e", neotree.focus, { desc = 'Open File [E]xplorer' })
-	end,
+		require("neo-tree").setup()
+		vim.keymap.set("n", "<Space>e", ":Neotree toggle<CR>", { desc = 'Open File [E]xplorer' })
+		-- vim.keymap.set("n", "<Space>e", ":Neotree focus<CR>", { desc = 'Open File [E]xplorer' })
+	end
+	-- config = function()
+	-- 	-- Unless you are still migrating, remove the deprecated commands from v1.x
+	-- 	local neotree = require 'neo-tree'
+	-- 	neotree.setup {
+	-- 		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+	-- 	}
+	-- 	vim.keymap.set("n", "<Space>e", neotree.focus, { desc = 'Open File [E]xplorer' })
+	-- end
 
 }
+
