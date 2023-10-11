0fa786b4856d32d6d836ba29b498b4d62e40f6fa
Juan Gajardo <jegajardog@gmail.com>
2023-10-11 13:32:04 -0300

    Little bug

--- init.lua
+++ init.lua
@@ -507,7 +507,7 @@ keymap('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]ear
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', , 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
