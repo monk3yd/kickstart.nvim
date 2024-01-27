d4a755aeec9abbe89c57b84f53d0972d2de0cd4b
Juan Gajardo <jegajardog@gmail.com>
2024-01-27 13:33:36 -0300

    Configure neo-tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,3 +1,5 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	version = "v3.x",
@@ -9,7 +11,14 @@ return {
 	config = function()
 		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-		require("neo-tree").setup()
+		require("neo-tree").setup({
+			close_if_last_window = false,
+			hide_root_node = true,
+			retain_hidden_root_indent = true,
+			filesystem = {
+				use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes instead of relying on nvim autocmd events.
+			}
+		})
 		vim.keymap.set("n", "<Space>e", ":Neotree toggle<CR>", { desc = 'Open File [E]xplorer' })
 		-- vim.keymap.set("n", "<Space>e", ":Neotree focus<CR>", { desc = 'Open File [E]xplorer' })
 	end
@@ -23,4 +32,3 @@ return {
 	-- end
 
 }
-
