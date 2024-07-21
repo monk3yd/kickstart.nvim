cae353ea9238ecbb7c4e3d2e5b804df3dea34a0d
Vladislav Grechannik <52157081+VlaDexa@users.noreply.github.com>
2024-07-21 22:34:17 +0200

    Make conform.nvim be lazy-loadable again (#977)
    The PR that disabled lazy loading (#818) was to fix plugin not being
    loaded before write. This sets up lazy to load conform before write.

--- init.lua
+++ init.lua
@@ -705,7 +705,8 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    lazy = false,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<leader>f',
