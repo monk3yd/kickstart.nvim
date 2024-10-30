99839fb05890f77986d3a1fa5580ef080836c4cf
gloomy-lemon-debatable <91877885+gloomy-lemon-debatable@users.noreply.github.com>
2024-10-30 17:01:42 +0000

    Set breakpoint icons and their highlight colors (#1194)
    * feat: Set breakpoint icons and their highlight colors
    
    * docs: Delete reference URL (written in PR)
    feat: "Break" and "Stop" arguments of vim.api.nvim_set_hl are changed because they are too common nouns
    feat: Comment out changes regarding diagnostic symbols so that only those who want to change them can do so
    
    ---------
    
    Co-authored-by: name <email>

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -93,6 +93,17 @@ return {
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    -- Change breakpoint icons
+    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    -- local breakpoint_icons = vim.g.have_nerd_font
+    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    -- for type, icon in pairs(breakpoint_icons) do
+    --   local tp = 'Dap' .. type
+    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    -- end
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
