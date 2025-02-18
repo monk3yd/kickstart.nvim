bcf2145e07499e73a53063db769525992bebf105
Joaquín Guerra <joaquinguerratocino@gmail.com>
2025-02-18 01:10:48 +0100

    perf: load tokyonight.nvim in the intended way (#1360)
    Fixes #1357

--- init.lua
+++ init.lua
@@ -952,15 +952,19 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 2000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
